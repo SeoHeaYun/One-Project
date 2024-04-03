f463be494b54480e02e9230d1c5ff9fd048e5a3f
hyejeong <hyejeong@hyejeongui-MacBookAir.local>
2024-04-04 00:15:40 +0900

    Feat: 더보기기능, 메인액티비티 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,19 +3,14 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.content.pm.PackageManager
-import android.graphics.drawable.GradientDrawable
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.ImageButton
+import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -72,9 +67,50 @@ class MainActivity : AppCompatActivity() {
                 requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
         }
+
+    //게시글부분
+        //게시글 글 더보기 관련
+        var long01 = findViewById<TextView>(R.id.long_text01)
+        var short01 = findViewById<TextView>(R.id.ddd01)
+        var long02 = findViewById<TextView>(R.id.long_text02)
+        var short02 = findViewById<TextView>(R.id.ddd02)
+        var long03 = findViewById<TextView>(R.id.long_text03)
+        var short03 = findViewById<TextView>(R.id.ddd03)
+        var long04 = findViewById<TextView>(R.id.long_text04)
+        var short04 = findViewById<TextView>(R.id.ddd04)
+
+        var id01 = findViewById<TextView>(R.id.top_id01)
+        id01.setOnClickListener {
+            var intent = Intent(this,DetailPageActivity::class.java)
+            intent.putExtra("userId",id01.text.toString())
+            startActivity(intent)
+        }
+
+        //더보기 기능 실행
+        setViewMore(long01, short01)
+        setViewMore(long02, short02)
+        setViewMore(long03, short03)
+        setViewMore(long04, short04)
     }
     private fun openGallery() {
         val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         pickImageLauncher.launch(gallery)
+    }
+
+    //더보기 기능
+    //getEllipsisCount : 해당라인의 생략된 길이를 반환함.
+    fun setViewMore(contentTextView: TextView, viewMoreTextView: TextView) {
+        contentTextView.post {
+            val lineCount = contentTextView.layout.lineCount
+            if (lineCount > 0) {
+                if (contentTextView.layout.getEllipsisCount(lineCount - 1) > 0) {
+                    viewMoreTextView.visibility = View.VISIBLE
+                    viewMoreTextView.setOnClickListener {
+                        contentTextView.maxLines = Int.MAX_VALUE
+                        viewMoreTextView.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 }
